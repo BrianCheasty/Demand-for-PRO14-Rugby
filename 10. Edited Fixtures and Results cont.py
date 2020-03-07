4010e63d2eeeffd1077190e3735ef1d2e65e7f6e
BCheasty <53042172+BCheasty@users.noreply.github.com>
2020-03-07 10:33:46 +0000

    Update 07032020

---
+++ 10. Edited Fixtures and Results cont.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+games=pd.read_csv('C:/Users/bcheasty/OneDrive - Athlone Institute Of Technology/Research Project/Data Set Creation/Data/Feature Creation/Feature Creation2.csv',encoding='latin-1')
