6790761bde39e0b345779dc9405ff2eb00e49093
BCheasty <53042172+BCheasty@users.noreply.github.com>
2020-03-12 21:49:11 +0000

    12032020

--- 10. Edited Fixtures and Results cont.py
+++ 10. Edited Fixtures and Results cont.py
@@ -109,66 +109,215 @@ league5.loc[league5['Season'].str.contains('2015/2016 Season|2016/2017 Season'),
 league5.loc[league5['Season'].str.contains('2017/2018 Season|2018/2019 Season'),'Table Difference']=(league5['Home Table Position']-league5['Away Table Position'])*2
 
 #x=league5['Home Total Points'].where(league5['Home Table Position']==3)
-league5.loc[((league5['Season'].str.contains('2015/2016|2016/2017'))&(league5['Home Table Position']==4)|(league5['Away Table Position']==4)),'Table Marker']=4
-league5.loc[((league5['Season'].str.contains('2017/2018|2018/2019'))&(league5['Home Table Position']==3)|(league5['Away Table Position']==3)),'Table Marker']=3
+league5.loc[((league5['Season'].str.contains('2015/2016|2016/2017'))&(league5['Home Table Position']==4)),'Table Marker']=4
+league5.loc[((league5['Season'].str.contains('2015/2016|2016/2017'))&(league5['Away Table Position']==4)),'Table Marker']=4
+league5.loc[((league5['Season'].str.contains('2015/2016|2016/2017'))&(league5['Home Table Position']==3)),'Table Marker']=3
+league5.loc[((league5['Season'].str.contains('2015/2016|2016/2017'))&(league5['Away Table Position']==3)),'Table Marker']=3
+league5.loc[((league5['Season'].str.contains('2017/2018|2018/2019'))&(league5['Home Table Position']==3)),'Table Marker']=3
+league5.loc[((league5['Season'].str.contains('2017/2018|2018/2019'))&(league5['Away Table Position']==3)),'Table Marker']=3
+league5.loc[((league5['Season'].str.contains('2017/2018|2018/2019'))&(league5['Home Table Position']==4)),'Table Marker']=4
+league5.loc[((league5['Season'].str.contains('2017/2018|2018/2019'))&(league5['Away Table Position']==4)),'Table Marker']=4
 
-def competitveness1517(row,x,y):
-    rounds=df2['Round']
+def competitveness1517(row,d):
+    rounds=row['Round']
     roundsleft=23-int(rounds)
     pointsleft=roundsleft*5
-    if (row['Home Total Points']+pointsleft)< x:
-        z=0
-    elif (row['Home Total Points']+pointsleft)> x:
-        if (row['Home Total Points']-pointsleft)> y:
-            z=0
+    if (row['Home Total Points']+pointsleft)< d:
+        z='Cant Qualify'
+    elif (row['Home Total Points']+pointsleft)> d:
+        if (row['Home Total Points']-pointsleft)> d:
+            z='Has Qualified'
         else:
-            z=1
+            z='Can Still Qualify'
     return z
-def competitveness1719(row,x,y):
+def competitveness1719(row,d):
     rounds=df2['Round']
     roundsleft=22-int(rounds)
     pointsleft=roundsleft*5
-    if (row['Home Total Points']+pointsleft)< x:
-        z=0
-    elif (row['Home Total Points']+pointsleft)> x:
-        if (row['Home Total Points']-pointsleft)> y:
-            z=0
+    if (row['Home Total Points']+pointsleft)< d:
+        z='Cant Qualify'
+    elif (row['Home Total Points']+pointsleft)> d:
+        if (row['Home Total Points']-pointsleft)> d:
+            z='Has Qualified'
         else:
-            z=1
+            z='Can Still Qualify'
     return z
 
 league5['Table Marker']=league5['Table Marker'].fillna(0)
 
-
+season22=['2015/2016 Season','2016/2017 Season']
+round22=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 league6=[]
-for i in season:
+for i in season22:
+    dflist=[]
     df=league5[(league5['Season'].str.contains(i))]
-    for j in rounds:
+    for j in round22:
         df2=df[(df['Round']==j)]
         dfcomb=[]
-        if len(df2)==0:
-            continue
+        a=df2[(df2['Table Marker']==4)]
+        print(len(a))
+        if len(a)==0:
+            a1=df2[(df2['Table Marker']==3)]
+            b=a1[['Home Total Points']]
+            c=list(b['Home Total Points'])
+            d=c[0]
+            print(d)
+            df2['Game Competitiveness']=df2.apply(lambda row: competitveness1517(row,d), axis=1 )
         else:
-            df3=df2[(df2['Season'].str.contains('2017/2018|2018/2019'))]
-            if len(df3)==0:
-                print('0')
-                a=df2[(df2['Table Marker']==4)]
-                b=a[['Home Total Points']]
-                c=list(b['Home Total Points'])
-                d=c[0]
-                print(d)
-                y=list(df2['Home Total Points'].where(df2['Table Marker']==5))
-                y=y[0]
-                df2['Game Competitiveness']=df2.apply(lambda row: competitveness1517(row,x,y), axis=1 )
-                dfcomb.append(df2)
-            else:
-                x=list(df3['Home Total Points'].where(df3['Table Marker']==3))
-                x=x[0]
-                y=list(df3['Home Total Points'].where(df3['Table Marker']==4))
-                y=y[0]
-                df3['Game Competitiveness']=df3.apply(lambda row: competitveness1719(row,x,y), axis=1 )
-                dfcomb.append(df3)
-        league6.append(dfcomb)
-                
+            b=a[['Home Total Points']]
+            c=list(b['Home Total Points'])
+            d=c[0]
+            print(d)
+            df2['Game Competitiveness']=df2.apply(lambda row: competitveness1517(row,d), axis=1 )
+        dflist.append(df2)
+    dfconcat=pd.concat(dflist)
+    league6.append(dfconcat)
+season22=['2017/2018 Season','2018/2019 Season']
+round22=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+for i in season22:
+    dflist=[]
+    df=league5[(league5['Season'].str.contains(i))]
+    for j in round22:
+        df2=df[(df['Round']==j)]
+        dfcomb=[]
+        a=df2[(df2['Table Marker']==4)]
+        print(len(a))
+        if len(a)==0:
+            a1=df2[(df2['Table Marker']==3)]
+            b=a1[['Home Total Points']]
+            c=list(b['Home Total Points'])
+            d=c[0]
+            print(d)
+            df2['Game Competitiveness']=df2.apply(lambda row: competitveness1517(row,d), axis=1 )
+        else:
+            b=a[['Home Total Points']]
+            c=list(b['Home Total Points'])
+            d=c[0]
+            print(d)
+            df2['Game Competitiveness']=df2.apply(lambda row: competitveness1517(row,d), axis=1 )
+        dflist.append(df2)
+    dfconcat=pd.concat(dflist)
+    league6.append(dfconcat)    
+league7=pd.concat(league6)
+        
+        
+league7['Date']=pd.to_datetime(league7['Date'])
+league7['Day Of Week']=league7['Date'].dt.dayofweek
+
+def time(row):
+    ko=row['Kick Off'].split(':')
+    ko2=int(ko[0])
+    if ko2 < 14:
+        x='Early'
+    elif ko2 >= 14:
+        if ko2 < 18:
+            x='Afternoon'
+        else:
+            x='Evening'
+    return x
+league7['Kick Off Hour']=league7.apply(lambda row:time(row), axis=1)    
+
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Leinster'))),'Number ofP14 Wins']=5
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Leinster'))),'Number ofP14 Wins']=4
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Leinster'))),'Number ofP14 Wins']=3
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Leinster'))),'Number ofP14 Wins']=3
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Ospreys'))),'Number ofP14 Wins']=4
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Ospreys'))),'Number ofP14 Wins']=4
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Ospreys'))),'Number ofP14 Wins']=4
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Ospreys'))),'Number ofP14 Wins']=4
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Munster'))),'Number ofP14 Wins']=3
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Munster'))),'Number ofP14 Wins']=3
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Munster'))),'Number ofP14 Wins']=3
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Munster'))),'Number ofP14 Wins']=3
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Scarlets'))),'Number ofP14 Wins']=2
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Scarlets'))),'Number ofP14 Wins']=2
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Scarlets'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Scarlets'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Ulster'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Ulster'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Ulster'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Ulster'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Connacht'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Connacht'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Connacht'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Connacht'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Glasgow'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Glasgow'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Glasgow'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Glasgow'))),'Number ofP14 Wins']=1
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Edinburgh'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Edinburgh'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Edinburgh'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Edinburgh'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Cardiff'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Cardiff'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Cardiff'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Cardiff'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Zebre'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Zebre'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Zebre'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Zebre'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Treviso'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Treviso'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Treviso'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Treviso'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Dragons'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Dragons'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Dragons'))),'Number ofP14 Wins']=0
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Dragons'))),'Number ofP14 Wins']=0
+
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Leinster'))),'Years sinceP14 Win']=0
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Leinster'))),'Years sinceP14 Win']=0
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Leinster'))),'Years sinceP14 Win']=3
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Leinster'))),'Years sinceP14 Win']=2
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Ospreys'))),'Years sinceP14 Win']=7
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Ospreys'))),'Years sinceP14 Win']=6
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Ospreys'))),'Years sinceP14 Win']=5
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Ospreys'))),'Years sinceP14 Win']=4
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Munster'))),'Years sinceP14 Win']=8
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Munster'))),'Years sinceP14 Win']=7
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Munster'))),'Years sinceP14 Win']=6
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Munster'))),'Years sinceP14 Win']=5
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Scarlets'))),'Years sinceP14 Win']=2
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Scarlets'))),'Years sinceP14 Win']=1
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Scarlets'))),'Years sinceP14 Win']=16
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Scarlets'))),'Years sinceP14 Win']=16
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Ulster'))),'Years sinceP14 Win']=13
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Ulster'))),'Years sinceP14 Win']=12
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Ulster'))),'Years sinceP14 Win']=11
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Ulster'))),'Years sinceP14 Win']=10
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Connacht'))),'Years sinceP14 Win']=3
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Connacht'))),'Years sinceP14 Win']=2
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Connacht'))),'Years sinceP14 Win']=1
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Connacht'))),'Years sinceP14 Win']=14
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Glasgow'))),'Years sinceP14 Win']=4
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Glasgow'))),'Years sinceP14 Win']=3
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Glasgow'))),'Years sinceP14 Win']=2
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Glasgow'))),'Years sinceP14 Win']=1
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Edinburgh'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Edinburgh'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Edinburgh'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Edinburgh'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Cardiff'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Cardiff'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Cardiff'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Cardiff'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Zebre'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Zebre'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Zebre'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Zebre'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Treviso'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Treviso'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Treviso'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Treviso'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2018/2019'))&(league7['Home Team'].str.contains('Dragons'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2017/2018'))&(league7['Home Team'].str.contains('Dragons'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2016/2017'))&(league7['Home Team'].str.contains('Dragons'))),'Years sinceP14 Win']=21
+league7.loc[((league7['Season'].str.contains('2015/2016'))&(league7['Home Team'].str.contains('Dragons'))),'Years sinceP14 Win']=21
+
+league7['Home Winning Percentage']=round((league7['Home Total Points']/league7['Total Points Avail TD'])*100)
+
+
+
 
 
